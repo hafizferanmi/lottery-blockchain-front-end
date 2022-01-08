ffdcbc01201b903622af4c599813bc50efb2f444
hafizferanmi <hafizferanmi@gmail.com>
2022-01-08 21:14:43 +0100

    Cleanup Utils test (functions)

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -7,30 +7,28 @@ import {
 } from "../utils";
 
 describe("Utils.Js", () => {
-  test("Convert WEI to HEX", () => {
+  test("Convert WEI to HEX / HEX to WEI", () => {
     const wei25000 = 25000;
     const hex25000 = "0x61a8";
 
     const hexValue = convertWeiToHex(wei25000);
     const weiValue = convertHexToWei(hex25000);
 
-    console.log(weiValue, "wei value");
-
     expect(hexValue).toBe(hex25000);
     expect(weiValue).toBe(wei25000);
   });
 
-  test("Convert Ether to Hex", async () => {
-    const ether = 0.2;
-    const expectedValue = "0x02c68af0bb140000";
+  test("Convert ETHER to HEX / HEX to ETHER", async () => {
+    const ether0Point2 = 0.2;
+    const hex0Point2 = "0x02c68af0bb140000";
 
-    const value = convertEtherValueToHex(ether);
-    const etherValue = convertHexToEther(expectedValue);
-    expect(value).toBe(expectedValue);
-    expect(ether).toBe(etherValue);
+    const hexValue = convertEtherValueToHex(ether0Point2);
+    const etherValue = convertHexToEther(hex0Point2);
+    expect(hexValue).toBe(hex0Point2);
+    expect(etherValue).toBe(ether0Point2);
   });
 
-  test("Format Wallet Balance", async () => {
+  test("Format ADDRESS Balance", async () => {
     const balance = formatBalance("0x82cd1272ec3dd11");
     expect(balance).toBe("0.5891");
   });
